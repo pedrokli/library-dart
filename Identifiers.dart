e7a213f8b07843243f9faca8ebbf6bd5e82cf4c7
Pedro Klimaitis <109765945+pedrokli@users.noreply.github.com>
2022-10-07 00:57:14 -0300

    Add Identifiers via upload
    How to work with variables in Dart

---
+++ Identifiers.dart
@@ -0,0 +1,16 @@
+void main() {
+  String name = 'Pedro Klimaitis Moreira da Silva';
+  String nickname = 'Klimarx';
+  int age = 26;
+  double height = 1.86;
+  bool geek = true;
+
+  String phrase = 'I am $nickname \n'
+      'But my fullname is $name, \n'
+      'Am I considered a geek? $geek. \n'
+      'I am $height meters high \n'
+      'And I am $age years old';
+
+  print(phrase);
+
+}
